d06f3e19257130de4feaad0c958c7f18e42ad9f7
qiu <1462909477@qq.com>
2024-11-06 22:25:33 +0800

    copy from lab5

--- lab6-aop-spring/src/main/java/com/qiu/ssm/annotation/aop/AfterReturning.java
+++ lab6-aop-spring/src/main/java/com/qiu/ssm/annotation/aop/AfterReturning.java
@@ -1,7 +1,12 @@
 package com.qiu.ssm.annotation.aop;
 
-import java.lang.annotation.Annotation;
+import java.lang.annotation.*;
 
+/**
+ * @author _qiu
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface AfterReturning {
     Class<? extends Annotation>[] annotation() default {};
     Class<? extends Annotation>[] within() default {};
